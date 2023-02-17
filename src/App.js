eecd94a6ccfaf62600656e28f15d5f54cecf91d1
dmr-911 <dewan.mizanur911@gmail.com>
2023-02-17 17:18:12 +0600

    home page updated design

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Register from "./pages/Register";
 
 function App() {
   return (
-    <div className="App">
+    <div className="body-wrapper">
       <Router>
         <Routes>
           <Route path="/">
