86b9fe0a76da06d590e61fcce27798413a7852a6
dmr-911 <dewan.mizanur911@gmail.com>
2023-02-17 17:24:16 +0600

    basic layout of home page updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Chat from "../components/Chat";
+import Sidebar from "../components/Sidebar";
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
-  )
-}
+    <div className="home">
+      <Sidebar />
+      <Chat />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
